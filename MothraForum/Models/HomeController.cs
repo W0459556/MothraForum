181eccb382c56f20235170a093dff8c8cdb9d021
W0459556 <w0459556@campus.nscc.ca>
2025-02-07 15:52:05 -0400

    feat: create base MVC program

---
+++ MothraForum/Models/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MothraForum.Models
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            // i created this accidentally and we are pretending we do not see it. agreed? agreed. nice disciussion. 
+        }
+    }
+}
