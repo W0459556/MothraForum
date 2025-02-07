042a22d99f440825b82de100586a971a32652fae
W0459556 <w0459556@campus.nscc.ca>
2025-02-07 17:38:54 -0400

    feat: add rudimentary views

--- MothraForum/Controllers/CommentController.cs
+++ MothraForum/Controllers/CommentController.cs
@@ -12,9 +12,110 @@ namespace MothraForum.Controllers
 {
     public class CommentController : Controller
     {
-        public IActionResult Index()
+        private readonly MothraForumContext _context;
+
+        public CommentController(MothraForumContext context)
         {
+            _context = context;
+        }
+
+        // GET: Comment/Create/5
+        public IActionResult Create(int discussionId)
+        {
+            ViewData["DiscussionId"] = discussionId;
             return View();
+        }
+
+        // POST: Comment/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Content,DiscussionId")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                comment.CreatedAt = DateTime.Now; // set creation date
+
+                _context.Add(comment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Details", "Discussion", new { id = comment.DiscussionId });
+            }
+            return View(comment);
+        }
+
+        // GET: Comment/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            return View(comment);
+        }
+
+        // POST: Comment/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CommentId,Content")] Comment comment)
+        {
+            if (id != comment.CommentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(comment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CommentExists(comment.CommentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Details", "Discussion", new { id = comment.DiscussionId });
+            }
+            return View(comment);
+        }
+
+        // GET: Comment/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var comment = await _context.Comments
+                .FirstOrDefaultAsync(m => m.CommentId == id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return View(comment);
+        }
+
+        // POST: Comment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment != null)
+            {
+                _context.Comments.Remove(comment);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Details", "Discussion", new { id = comment.DiscussionId });
+        }
+
+        private bool CommentExists(int id)
+        {
+            return _context.Comments.Any(e => e.CommentId == id);
         }
     }
 }
--- MothraForum/Controllers/VoteController.cs
+++ MothraForum/Controllers/VoteController.cs
@@ -12,9 +12,28 @@ namespace MothraForum.Controllers
 {
     public class VoteController : Controller
     {
-        public IActionResult Index()
+        private readonly MothraForumContext _context;
+
+        public VoteController(MothraForumContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // POST: Vote/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(int discussionId, int value)
+        {
+            var vote = new Vote
+            {
+                DiscussionId = discussionId,
+                Value = value
+            };
+
+            _context.Add(vote);
+            await _context.SaveChangesAsync(); // add vote & save
+
+            return RedirectToAction("Details", "Discussion", new { id = discussionId }); // Redirect to the discussion details page
         }
     }
 }
